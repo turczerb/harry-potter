73eec0de8bf98b09608bf883cb18a9ec3e8a08a2
turczerb <turczerb@gmail.com>
2023-08-24 12:25:03 +0200

    data develop

--- src/components/Books/Book.jsx
+++ src/components/Books/Book.jsx
@@ -1,8 +1,27 @@
-const Book = (props) => {
+import { Link } from "react-router-dom"; //tudjunk másik oldalra jump
+import styled from "styled-components"; //css
+
+//ez az ami az egész 1db könyv kártyát tartalmazza
+const ProductCard = styled.div``;
+
+const ProductCardInner = styled.div``;
+
+const Book = ({ item }) => {
   return (
-    <div>
-      <div>Book</div>
-    </div>
+    <ProductCard>
+      <ProductCardInner>
+        <a>
+          <div>
+            <div>
+              <div>kép</div>
+            </div>
+            <div>
+              <h4></h4>
+            </div>
+          </div>
+        </a>
+      </ProductCardInner>
+    </ProductCard>
   );
 };
 
--- src/components/Books/BookData.jsx
+++ src/components/Books/BookData.jsx
@@ -3,65 +3,72 @@ export const BookData = [
     type: "Harry Potter",
     bookTypes: [
       {
+        path: "books/philosophers-stone",
         writer: "J.K. Rowling",
         title: "Harry Potter and the Philosopher's Stone",
         year: 1997,
         illustrator: "Thomas Taylor",
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle. Then, on Harry’s eleventh birthday, a great beetle-eyed giant of a man called Rubeus Hagrid bursts in with some astonishing news: Harry Potter is a wizard, and he has a place at Hogwarts School of Witchcraft and Wizardry. An incredible adventure is about to begin!",
       },
       {
+        path: "books/chamber-of-secrets",
         writer: "J.K. Rowling",
         title: "Harry Potter and the Chamber of Secrets",
         year: 1998,
         illustrator: "Thomas Taylor",
-        image: "",
+        image: "src/images/hp2.jpg",
         story:
           "Harry Potter's summer has included the worst birthday ever, doomy warnings from a house-elf called Dobby, and rescue from the Dursleys by his friend Ron Weasley in a magical flying car! Back at Hogwarts School of Witchcraft And Wizardry for his second year, Harry hears strange whispers echo through empty corridors – and then the attacks start. Students are found as though turned to stone … Dobby’s sinister predictions seem to be coming true.",
       },
       {
+        path: "books/prisoners-of-azkaban",
         writer: "J.K. Rowling",
         title: "Harry Potter and the Prisoner of Azkaban",
         year: 1999,
         illustrator: "Thomas Taylor",
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "When the Knight Bus crashes through the darkness and screeches to a halt in front of him, it’s the start of another far from ordinary year at Hogwarts for Harry Potter. Sirius Black, escaped mass-murderer and follower of Lord Voldemort, is on the run – and they say he is coming after Harry. In his first ever Divination class, Professor Trelawney sees an omen of death in Harry’s tea leaves... But perhaps most terrifying of all are the Dementors patrolling the school grounds, with their soul-sucking kiss.",
       },
       {
+        path: "books/goblet-of-fire",
         writer: "J.K. Rowling",
         title: "Harry Potter and the Goblet of Fire",
         year: 2000,
         illustrator: "Thomas Taylor",
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "The Triwizard Tournament is to be held at Hogwarts. Only wizards who are over seventeen are allowed to enter – but that doesn’t stop Harry dreaming that he will win the competition. Then at Hallowe’en, when the Goblet of Fire makes its selection, Harry is amazed to find his name is one of those that the magical cup picks out. He will face death-defying tasks, dragons and Dark wizards, but with the help of his best friends, Ron and Hermione, he might just make it through – alive!",
       },
       {
+        path: "books/order-of-the-phoenix",
         writer: "J.K. Rowling",
         title: "Harry Potter and the Order of the Phoenix",
         year: 2003,
         illustrator: "Thomas Taylor",
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "Dark times have come to Hogwarts. After the Dementors’ attack on his cousin Dudley, Harry Potter knows that Voldemort will stop at nothing to find him. There are many who deny the Dark Lord’s return, but Harry is not alone: a secret Order gathers at Grimmauld Place to fight against the Dark forces. Harry must allow Professor Snape to teach him how to protect himself from Voldemort’s savage assaults on his mind. But they are growing stronger by the day and Harry is running out of time.",
       },
       {
+        path: "books/the-half-blood-prince",
         writer: "J.K. Rowling",
         title: "Harry Potter and the Half-Blood Prince",
         year: 2005,
         illustrator: "Thomas Taylor",
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "When Dumbledore arrives at Privet Drive one summer night to collect Harry Potter, his wand hand is blackened and shrivelled, but he does not reveal why. Secrets and suspicion are spreading through the wizarding world, and Hogwarts itself is not safe. Harry is convinced that Malfoy bears the Dark Mark: there is a Death Eater amongst them. Harry will need powerful magic and true friends as he explores Voldemort’s darkest secrets, and Dumbledore prepares him to face his destiny.",
       },
       {
+        path: "books/the-deadthly-hallows",
         writer: "J.K. Rowling",
         title: "Harry Potter and the Deathly Hallows",
         year: 2007,
         illustrator: "Thomas Taylor",
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "As he climbs into the sidecar of Hagrid's motorbike and takes to the skies, leaving Privet Drive for the last time, Harry Potter knows that Lord Voldemort and the Death Eaters are not far behind. The protective charm that has kept Harry safe until now is broken, but he cannot keep hiding. The Dark Lord is breathing fear into everything Harry loves, and to stop him Harry will have to find and destroy the remaining Horcruxes. The final battle must begin – Harry must stand and face his enemy.",
       },
@@ -71,26 +78,29 @@ export const BookData = [
     type: "Fantastic Beasts",
     bookTypes: [
       {
+        path: "books/fantastic-beasts-and-where-to-find-them",
         writer: "J.K. Rowling",
         title: "Fantastic Beasts and Where to Find Them",
         year: 2001,
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "J.K. Rowling invites you to explore a new era of the Wizarding World . . .When Magizoologist Newt Scamander arrives in New York, he intends his stay to be just a brief stopover. However, when his magical case is misplaced and some of Newt’s fantastic beasts escape, it spells trouble for everyone . . .Inspired by the original Hogwart’s textbook by Newt Scamander, Fantastic Beasts and Where to Find Them: The Original screenplay marks the screenwriting debut of J.K. Rowling, author of the beloved and internationally bestselling Harry Potter books. A feat of imagination and featuring a cast of remarkable characters and magical creatures, this is epic adventure-packed storytelling at its very best. Whether an existing fan or new to the wizarding world, this is a perfect addition for any film lover or reader’s bookshelf.",
       },
       {
+        path: "books/fantastic-beasts-the-crimes-of-grindelwald",
         writer: "J.K. Rowling",
         title: "Fantastic Beasts: The Crimes of Grindelwald",
         year: 2018,
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "The Wizarding World journey continues . . . The powerful Dark wizard Gellert Grindelwald was captured in New York with the help of Newt Scamander. But, making good on his threat, Grindelwald escapes custody and sets about gathering followers, most of whom are unsuspecting of his true agenda: to raise pure-blood wizards up to rule over all non-magical beings. In an effort to thwart Grindelwald’s plans, Albus Dumbledore enlists Newt, his former Hogwarts student, who agrees to help once again, unaware of the dangers that lie ahead. Lines are drawn as love and loyalty are tested, even among the truest friends and family, in an increasingly divided wizarding world. Fantastic Beasts: The Crimes of Grindelwald is the second screenplay in a five-film series to be written by J.K. Rowling, author of the internationally bestselling Harry Potter books. Set in 1927, a few months after the events of Fantastic Beasts and Where To Find Them, and moving from New York to London, Paris and even back to Hogwarts, this story of mystery and magic reveals an extraordinary new chapter in the wizarding world. Illustrated with stunning line art from MinaLima with some surprising nods to the Harry Potter stories that will delight fans of both the books and films.",
       },
       {
+        path: "books/fantastic-beasts-the-sercrets-of-dumbi",
         writer: "J.K. Rowling",
         title: "Fantastic Beasts: The Secrets of Dumbledore",
         year: 2018,
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "Professor Albus Dumbledore knows the powerful Dark wizard Gellert Grindelwald is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts Magizoologist Newt Scamander to lead an intrepid team of wizards, witches, and one brave Muggle baker on a dangerous mission, where they encounter old and new beasts and clash with Grindelwald’s growing legion of followers. But with the stakes so high, how long can Dumbledore remain on the sidelines?",
       },
@@ -100,26 +110,29 @@ export const BookData = [
     type: "Hogwarts Library",
     bookTypes: [
       {
+        path: "books/fantastic-beasts-and where-to-find-them-schoolbook",
         writer: "J.K. Rowling",
         title: "Fantastic Beasts and Where to Find Them",
         year: 2001,
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "J.K. Rowling invites you to explore a new era of the Wizarding World . . .When Magizoologist Newt Scamander arrives in New York, he intends his stay to be just a brief stopover. However, when his magical case is misplaced and some of Newt’s fantastic beasts escape, it spells trouble for everyone . . .Inspired by the original Hogwart’s textbook by Newt Scamander, Fantastic Beasts and Where to Find Them: The Original screenplay marks the screenwriting debut of J.K. Rowling, author of the beloved and internationally bestselling Harry Potter books. A feat of imagination and featuring a cast of remarkable characters and magical creatures, this is epic adventure-packed storytelling at its very best. Whether an existing fan or new to the wizarding world, this is a perfect addition for any film lover or reader’s bookshelf.",
       },
       {
+        path: "books/quidditch-through-the-ages",
         writer: "J.K. Rowling",
         title: "Quidditch Through the Ages",
         year: 2001,
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "A perennial bestseller in the wizarding world and one of the most popular books in the Hogwarts School library, Quidditch Through the Ages contains all you will ever need to know about the history, the rules – and the breaking of the rules – of the noble sport of Quidditch. Packed with fascinating facts, this definitive guide by the esteemed Quidditch writer Kennilworthy Whisp charts the game's history from its early origins in the medieval mists on Queerditch Marsh, through to the modern-day sport loved by so many wizard and Muggle families around the world. With comprehensive coverage of famous Quidditch teams, the commonest fouls, the development of racing brooms, and much more, this is a must-have sporting bible for all Harry Potter fans, Quidditch lovers and players, whether the weekend amateur or the seasoned Chudley Cannons season-ticket holder. A contribution from the sale of each book will go to Comic Relief.",
       },
       {
+        path: "books/the-tales-of-beedle-the-bard",
         writer: "J.K. Rowling",
         title: "The Tales of Beedle The Bard",
         year: 2008,
-        image: "",
+        image: "src/images/hp1.jpg",
         story:
           "The Tales of Beedle the Bard have been favourite bedtime reading in wizarding households for centuries. Full of magic and trickery, these classic tales both entertain and instruct, and remain as captivating to young wizards today as they were when Beedle first put quill to parchment in the fifteenth century. There are five tales in all: 'The Tale of the Three Brothers' Harry Potter fans will know from Harry Potter and the Deathly Hallows; 'The Fountain of Fair Fortune', 'The Warlock's Hairy Heart', 'The Wizard and the Hopping Pot' and 'Babbitty Rabbitty and her Cackling Stump' complete the collection. These narrative gems are accompanied by explanatory notes by Professor Albus Dumbledore (included by kind permission of the Hogwarts Headmaster's archive). His illuminating thoughts reveal the stories to be much more than just simple moral tales, and are sure to make Babbitty Rabbitty and the slug-belching Hopping Pot as familiar to Muggles as Snow White and Cinderella. ",
       },
