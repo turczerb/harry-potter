195afd1d37078fffc9d8461aaa4d6b75b0eabb86
turczerb <turczerb@gmail.com>
2023-08-24 16:52:08 +0200

    Book component css

--- src/components/Books/Book.jsx
+++ src/components/Books/Book.jsx
@@ -7,7 +7,17 @@ const ProductCard = styled.div``;
 
 const ProductCardInner = styled.div``;
 
-const KlikkContainer = styled(Link)``;
+const KlikkContainer = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
+
+const Nemtom = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Picture = styled.img`
   height: 400px;
@@ -19,14 +29,14 @@ const Book = ({ item }) => {
     <ProductCard>
       <ProductCardInner>
         <KlikkContainer to={item.path}>
-          <div>
+          <Nemtom>
             <div>
               <Picture src={img} alt="pic"></Picture>
             </div>
             <div>
               <h4>{item.title}</h4>
             </div>
-          </div>
+          </Nemtom>
         </KlikkContainer>
       </ProductCardInner>
     </ProductCard>
