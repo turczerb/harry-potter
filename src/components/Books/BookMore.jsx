c2ab86baff0affb776159fec2a72e52232afffc2
turczerb <turczerb@gmail.com>
2023-08-28 14:21:09 +0200

    scrollToTop Component

--- src/components/Books/BookMore.jsx
+++ src/components/Books/BookMore.jsx
@@ -1,9 +1,42 @@
 import styled from "styled-components"; //css
 import { useParams } from "react-router-dom";
 import { BookData } from "./BookData";
+const images = require.context("../../images");
 
 const OutContainer = styled.div`
   padding-top: 200px;
+  background-color: black;
+  dispay: flex;
+  flex-direction: column;
+`;
+
+const InnerContainer = styled.div``;
+
+const Title = styled.div`
+  color: white;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 30px;
+`;
+
+const Stori = styled.div`
+  color: white;
+  font-family: "Sofia Pro", serif;
+  font-size: 24px;
+  padding: 10px;
+  max-width: 45em;
+  //text-align: center;
+  text-align: left;
+  margin: 0.55556em auto 0px;
+  letter-spacing: 0.5px;
+  line-height: 36px;
+`;
+
+const Picture = styled.img`
+  height: 600px;
+  border-radius: 13px;
 `;
 
 function findBook(title) {
@@ -20,11 +53,23 @@ const BookMore = () => {
   const { title } = useParams();
   //const thisBook = BookData[0].bookTypes.find((prod) => prod.title === title);
   const item = findBook(title);
+  let img = images(item.image);
 
   return (
     <OutContainer>
-      <div>{item.title} </div>
-      <div>{item.story} </div>
+      <InnerContainer>
+        <Title>
+          <div></div>
+          <div>
+            <Picture src={img} alt="pic"></Picture>
+          </div>
+          <div>
+            <h1>{item.title}</h1>
+          </div>
+          <div></div>
+        </Title>
+      </InnerContainer>
+      <Stori>{item.story} </Stori>
     </OutContainer>
   );
 };
